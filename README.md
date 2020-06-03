73f5c6d21e9289b3a670836395a97e5d4f27148f
serknight <chrisknight.mail@gmail.com>
2020-06-03 17:22:44 -0600

    Xero Ruby script to migrate OAuth1.0a tokens to OAuth2.0 tokens

---
+++ README.md
@@ -0,0 +1,18 @@
+# Xero Ruby Token Migration Script
+
+This project shows a low level breakdown of how to make an authorized XeroAPI call to [migrate your XeroAPI connection to OAuth2.0](https://developer.xero.com/documentation/oauth2/migrate).
+
+To use this to migrate your XeroAPI tokens you need to configure the following:
+> Requires `ruby 2.7.0` to run.
+
+* Rename `privatekey.pem.sample` to `privatekey.pem` and replace the contents with your actual private key
+* Replace the contents of `oauth1_tokens.json` with an array of valid OAuth1.0a tokens ( Tokens must be refreshed prior to exchanging them for Oauth2.0 tokens )
+* Replace the following constant values with your own:
+  * CONSUMER_KEY = 'YOUR_CONSUMER_KEY'
+  * SCOPES = 'offline_access SCOPE_1 SCOPE_2 SCOPE_3'
+  * OAUTH2_CLIENT_ID = 'YOUR_OAUTH2_CLIENT_ID'
+  * OAUTH2_CLIENT_SECRET = 'YOUR_OAUTH2_CLIENT_SECRET'
+
+Running the script `ruby migration.rb` will output your new OAuth2.0 token_sets in a file called `oauth2_tokens.json` and it will also print out JSON of your converted tokens.
+
+After that you will need to get those `token_sets` into your software system and configure your new API calls to use the new `access_token`, `xero_tenant_id`, and `refresh_token` to persist offline access indefinitely.
---
+++ migration.rb
@@ -0,0 +1,123 @@
+require 'securerandom'
+require 'base64'
+require 'openssl'
+require 'digest/sha1'
+require 'uri'
+require 'json'
+require 'net/http'
+require 'csv'
+
+CONSUMER_KEY = 'YOUR_CONSUMER_KEY'
+SCOPES = 'offline_access SCOPE_1 SCOPE_2 SCOPE_3'
+OAUTH2_CLIENT_ID = 'YOUR_OAUTH2_CLIENT_ID'
+OAUTH2_CLIENT_SECRET = 'YOUR_OAUTH2_CLIENT_SECRET'
+
+SIGNATURE_METHOD = 'RSA-SHA1'
+ENDPOINT = 'https://api.xero.com/oauth/migrate'
+NONCE = SecureRandom.uuid
+TIMESTAMP = Time.now.getutc.to_i.to_s
+
+class XeroOauthMigration
+
+  def self.encode_uri_component(data)
+    URI.encode_www_form_component(data)
+  end
+  
+  def self.get_oauth_params
+    oauth_params = {}
+    oauth_params['oauth_consumer_key'] = CONSUMER_KEY
+    oauth_params['oauth_nonce'] = NONCE
+    oauth_params['oauth_signature_method'] = SIGNATURE_METHOD
+    oauth_params['oauth_timestamp'] = TIMESTAMP
+    oauth_params['oauth_token'] = @token
+    oauth_params['oauth_version'] = '1.0'
+    oauth_params['tenantType'] = 'ORGANISATION'
+  
+    oauth_params
+  end
+  
+  def self.to_oauth_param_string(params)
+    # lexigraphically sorted params Xero is expecting
+    "oauth_consumer_key=#{params['oauth_consumer_key']}" +
+    "&oauth_nonce=#{params['oauth_nonce']}" +
+    "&oauth_signature_method=#{params['oauth_signature_method']}" +
+    "&oauth_timestamp=#{params['oauth_timestamp']}" +
+    "&oauth_token=#{params['oauth_token']}" +
+    "&oauth_version=#{params['oauth_version']}" +
+    "&tenantType=#{params['tenantType']}"
+  end
+  
+  def self.get_signature_base_string(http_method, param_string)
+    "#{http_method.upcase}&" + # Uppercase HTTP method
+    "#{encode_uri_component(ENDPOINT)}&" + # Base URI
+    encode_uri_component(param_string).to_s # OAuth parameter string
+  end
+  
+  def self.sign_signature_base_string(sbs, signing_key)
+    digest = OpenSSL::Digest::SHA1.new
+    rsa_key = OpenSSL::PKey::RSA.new signing_key
+    signature = ''
+    begin
+      signature = rsa_key.sign(digest, sbs)
+    rescue
+      raise Exception, 'Unable to sign the signature base string.'
+    end
+  
+    Base64.strict_encode64(signature).chomp.gsub(/\n/, '')
+  end
+  
+  def self.get_authorization_string(oauth_params)
+    header = 'OAuth '
+    oauth_params.each {|entry|
+      entry_key = entry[0]
+      entry_val = entry[1]
+      header = "#{header}#{entry_key}='#{entry_val}',"
+    }
+    header.slice(0, header.length - 1) # Remove trailing ,
+  end
+
+  def self.migrate_token(token)
+    @token = token
+    # 1) Setup your base signature
+    oauth_params = get_oauth_params
+    param_string = to_oauth_param_string(oauth_params)
+    base_signature_string = get_signature_base_string('POST', param_string)
+
+    # 2) Sign that with your private key
+    signing_key = File.read('./privatekey.pem')
+    signature = sign_signature_base_string(base_signature_string, signing_key)
+    oauth_params['oauth_signature'] = encode_uri_component(signature)
+
+    # 3) Build your authorization headers & POST params
+    authorization_header = get_authorization_string(oauth_params)
+    params = {
+      "scope": "#{SCOPES}",
+      "client_id": "#{OAUTH2_CLIENT_ID}",
+      "client_secret": "#{OAUTH2_CLIENT_SECRET}"
+    }
+
+    # 4) Make your API call
+    uri = URI.parse(ENDPOINT)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    headers = {'Content-Type' =>'application/json', 'Authorization': authorization_header}
+    request = Net::HTTP::Post.new(uri.request_uri, headers)
+    request.body = params.to_json
+    response = http.request(request)
+
+    return response.body
+  end
+end
+
+tokens_to_migrate = JSON.parse(File.read('./oauth1_tokens.json'))
+new_tokens = []
+tokens_to_migrate.each do |oauth1_token|
+  new_tokens << XeroOauthMigration.migrate_token(oauth1_token['token'])
+end
+
+File.open("oauth2_tokens.json","w") do |f|
+  new_token_json = JSON.pretty_generate(new_tokens)
+  puts new_token_json
+  f.puts new_token_json
+end
